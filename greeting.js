c4474c04bd7087897d674d021ba5448c959563c6
brychxpin <bryan.oliveros17@tectijuana.edu.mx>
2022-08-13 20:07:17 -0700

    Module Exports

---
+++ greeting.js
@@ -0,0 +1,12 @@
+const greeting = (name) =>{
+    return 'hello friends ' + name;
+};
+const age = (age) =>{
+    return 'you are ' + age + ' years old';
+}
+
+
+module.exports = {
+    greeting,
+    age
+}
---
+++ moduleExports.js
@@ -0,0 +1,4 @@
+const data = require('./greeting.js');
+
+console.log(data.age(20));
+console.log(data.greeting('fernando'));
